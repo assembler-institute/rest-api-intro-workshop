4c8e72aff02c73bffbe43b97df8757b6f0f3ff01
Brahim <brahimcasas@hotmail.es>
2021-08-05 16:27:47 +0200

    add: user model

---
+++ src/models/user-model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "The email is required"],
+      trim: true,
+      unique: true,
+      validate: {
+        validator: (value) => validator.isEmail(value),
+        message: (props) => `The email ${props.value} is not valid`,
+      },
+    },
+    password: {
+      type: String,
+      unique: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
